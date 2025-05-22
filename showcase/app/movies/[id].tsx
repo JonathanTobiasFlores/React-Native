0e4a293b694999ead1bc4d4c9c689989da2a1992
Jonathan Flores <jonathan.tobias.flores@gmail.com>
2025-05-23 00:19:46 +0200

    Add template layout

---
+++ showcase/app/movies/[id].tsx
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const movieDetails = () => {
+  return (
+    <View>
+      <Text>Movie Details</Text>
+    </View>
+  )
+}
+
+export default movieDetails
